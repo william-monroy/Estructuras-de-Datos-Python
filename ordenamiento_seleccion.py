fb1d5f821dd7471a233304bcd7519260450a3956
William Monroy <A00829796@itesm.mx>
2021-04-01 23:47:48 -0500

    ordenamiento por seleccion

--- ordenamiento_seleccion.py
+++ ordenamiento_seleccion.py
@@ -1,4 +1,23 @@
 '''
+Ordenamiento por Seleccion
 
-
+Funcionamiento:
+- Buscar el dato mas pequeño de la lista
+- Intercambiarlo por el actual
+- Seguir buscando el dato mas pequeño de la lista
+- Intercambiarlo por el actual
+- Esto se repetira sucesivamente
 '''
+
+lista = [4,2,6,8,5,7,0]
+
+for i in range(len(lista)):
+    minimo = i
+    for j in range(i,len(lista)):
+        if lista[j] < lista[minimo]:
+            minimo = j
+    aux = lista[i]
+    lista[i] = lista[minimo]
+    lista[minimo] = aux
+
+print(lista)
